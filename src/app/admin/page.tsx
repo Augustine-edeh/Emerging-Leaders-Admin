53fb95cf52258fa51187cea78600e1e93179d382
Augustine Edeh <info.augustinesedeh@gmail.com>
2025-06-30 19:23:18 +0100

    Refactor ContentManagementTable component to fetch invoices from API and remove hardcoded data; update styles in globals.css for secondary colors.

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -3,7 +3,7 @@ import ContentManagementTable from "@/components/ContentManagementTable";
 
 const ContentManagementPage = () => {
   return (
-    <div className="flex-1 flex flex-col gap-8 min-h-0 bg-gray-500">
+    <div className="flex-1 flex flex-col gap-8 min-h-0 bg-gray-500 pb-2">
       <section className="flex justify-between items-center bg-pink-400">
         <div>
           <h1 className="text-2xl font-bold">Content Management</h1>
@@ -14,8 +14,8 @@ const ContentManagementPage = () => {
         <Button>Add New Content</Button>
       </section>
 
-      <section className="flex-1 min-h-0 flex flex-col gap-2 bg-emerald-600 p-1 rounded-xl">
-        <div className="flex justify-between bg-zinc-400">
+      <section className="flex-1 min-h-0 flex flex-col gap-2 pb-2 bg-lime-600 rounded-xl border border-border-secondary">
+        <div className="flex justify-between px-6 py-2.5">
           <h4 className="">Content List</h4>
 
           <div className="flex gap-2">
@@ -24,7 +24,7 @@ const ContentManagementPage = () => {
           </div>
         </div>
 
-        <div className="flex-1 min-h-0 rounded-t-xl bg-yellow-300 overflow- auto">
+        <div className="flex-1 min-h-0 rounded-xl bg-white border border-border-secondary p-5">
           {/* {Array.from({ length: 50 }, (_, index) => (
             <p key={index}>This is paragraph {index + 1}</p>
           ))} */}
