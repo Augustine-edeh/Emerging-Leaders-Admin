ea9ce5002385d932e231a8636c474ae96170eaf3
Augustine Edeh <info.augustinesedeh@gmail.com>
2025-07-02 06:58:46 +0100

    fix(TableActions): improve formatting and add separators for better menu organization

--- src/components/admin/TableActions.tsx
+++ src/components/admin/TableActions.tsx
@@ -3,6 +3,7 @@ import {
   DropdownMenuTrigger,
   DropdownMenuContent,
   DropdownMenuItem,
+  DropdownMenuSeparator,
 } from "@/components/ui/dropdown-menu";
 import { Archive, MoreVertical, PenLine, Trash2 } from "lucide-react";
 import { Button } from "@/components/ui/button";
@@ -12,7 +13,7 @@ const TableActions = () => {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" size="icon">
-          <MoreVertical className="h-4 w-4" />
+          <MoreVertical className="size-4" />
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
@@ -21,8 +22,11 @@ const TableActions = () => {
           className="flex items-center gap-2.5"
         >
           <PenLine />
-          <p> Edit Content</p>
+          <p>Edit Content</p>
         </DropdownMenuItem>
+
+        <DropdownMenuSeparator />
+
         <DropdownMenuItem
           onClick={() => console.log("Edit")}
           className="flex items-center gap-2.5"
@@ -30,6 +34,9 @@ const TableActions = () => {
           <Archive />
           <p>Archive Content</p>
         </DropdownMenuItem>
+
+        <DropdownMenuSeparator />
+
         <DropdownMenuItem
           onClick={() => console.log("Delete")}
           className="text-error flex items-center gap-2.5"
@@ -41,4 +48,5 @@ const TableActions = () => {
     </DropdownMenu>
   );
 };
+
 export default TableActions;
