4a395caced56e342ecd6f4d5603cc511204a2c75
Augustine Edeh <info.augustinesedeh@gmail.com>
2025-08-06 17:20:42 +0100

    fix(deployment-preview): remove unused font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Poppins } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 const poppins = Poppins({
   variable: "--font-poppins",
@@ -30,9 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${poppins.variable} ${poppins.className}`}
-      >
+      <body className={`${poppins.variable} ${poppins.className}`}>
         {children}
       </body>
     </html>
