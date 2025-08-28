cae8ae17f5736afe22018029bf150f8f2f15df32
Augustine Edeh <info.augustinesedeh@gmail.com>
2025-08-28 17:10:32 +0100

    chore: remove unused button componnet

--- src/app/admin/profile/page.tsx
+++ src/app/admin/profile/page.tsx
@@ -7,7 +7,6 @@ import {
   // showCustomErrorToast, NOTE: To be used  later during intergration onError for API calls
   showCustomSuccessToast,
 } from "@/components/toast/customToast";
-import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import UserAvatar from "@/components/UserAvatar";
 import clsx from "clsx";
